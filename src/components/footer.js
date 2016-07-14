41c442d042350de34aa14f2d4980fadfcc06106e
kiresuah <kiresuah@gmail.com>
2016-07-13 22:29:50 -0400

    basic simon board css configured

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer" onClick={() => window.open('https://github.com/kiresuah/react-tictactoe', '_blank')}>
+    <div className="footer" onClick={() => window.open('https://github.com/kiresuah/react-simon', '_blank')}>
       <img height="55" src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" alt="github" />
       <span id="info-tab">Application created by Erik Niehaus</span>
     </div>
